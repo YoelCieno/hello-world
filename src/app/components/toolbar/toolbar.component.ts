d704512da4425528ba6f4fc495b85ae850491359
frmonteira <frmonteira@gmail.com>
2018-12-02 04:58:27 +0100

    frmonteira :: ad webpack to import all the scss components

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -3,7 +3,6 @@ import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ToolbarComponent implements OnInit {
