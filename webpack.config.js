174230a511f5ab2f3fbc4d18f3dcb4de4b4d35cb
frmonteira <frmonteira@gmail.com>
2018-12-03 23:14:25 +0100

    frmonteira :: update ng to cng to add a webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -9,29 +9,29 @@ module.exports = {
     },
     module: {
       rules: [{
-          test: /\.scss$/,
-          use: [
-              // fallback to style-loader in development
-              process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-              "style-loader", // creates style nodes from JS strings
-              "css-loader", // translates CSS into CommonJS
-              "sass-loader", // compiles Sass to CSS, using Node Sass by default
-              {
-                loader: 'sass-resources-loader',
-                options: {
-                  // Provide path to the file/s with resources
-                  resources: require(path.join(process.cwd(), "src/app/styles/utils.js")),
-                },
-              },              
-          ]
+        test: /\.scss$/,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader", // compiles Sass to CSS, using Node Sass by default
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              // Provide path to the file/s with resources
+              resources: require(path.join(process.cwd(), "src/app/styles/utils.js")),
+            },
+          },              
+        ]
       }]
     },  
     plugins: [
       new MiniCssExtractPlugin({
-          // Options similar to the same options in webpackOptions.output
-          // both options are optional
-          filename: "[name].css",
-          chunkFilename: "[id].css"
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css"
       })
     ]       
   }
